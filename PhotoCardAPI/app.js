59f334232c2cb1532fdea70ff3520b1308b6eca2
Usenmfon <usenmfonuko@gmail.com>
2026-05-26 13:33:49 +0100

    classwork solved

---
+++ PhotoCardAPI/app.js
@@ -0,0 +1,36 @@
+
+fetch('https://jsonplaceholder.typicode.com/photos')
+      .then(response => response.json())
+      .then(json => displayCard(json.filter((item, index) => index < 10)))
+
+function displayCard(photos)
+{
+   for(let i = 0; i < photos.length; i++)
+   {
+    let card = document.createElement('div')
+    card.classList.add('card')
+
+    let id = document.createElement('p')
+    let title = document.createElement('p')
+    let image = document.createElement('img')
+
+    id.textContent = `ID: ${photos[i].id}`
+    title.textContent = `Title: ${photos[i].title}`
+    // image.src = photos[i].url
+    image.src = 'https://storage.googleapis.com/cdn4.thedogapi.com/optimized/8i1u7eq1KM.jpg'
+
+
+    card.appendChild(id)
+    card.appendChild(title)
+    card.appendChild(image)
+    document.body.appendChild(card)
+   }
+}
+
+
+
+// let fruits = ['apple', 'orange', 'banana', 'grape', 'mango']
+
+// let result = fruits.filter((item, index) => index < 3)
+
+// console.log(result)
